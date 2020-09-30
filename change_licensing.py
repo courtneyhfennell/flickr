f2ee7706b37ed8ec437d75a86caae4fe567245ea
Lewis <chlewis@dot.soa.alaska.gov>
2020-09-30 13:55:54 -0800

    App authorization

--- change_licensing.py
+++ change_licensing.py
@@ -1,5 +1,26 @@
+
+import config
+import flickrapi
+import webbrowser
+
 def main():
-    pass
+	
+	flickr = flickrapi.FlickrAPI(config.API_KEY, config.SECRET)
+	
+	print('Step 1: authenticate')
+	# Get a request token
+	flickr.get_request_token(oauth_callback='oob')
+
+	# Open a browser at the authentication URL
+	authorize_url = flickr.auth_url(perms='write')
+	webbrowser.open_new_tab(authorize_url)
+
+	# Get the verifier code from the user
+	verifier = str(input('Verifier code: '))
+
+	# Trade the request token for an access token
+	flickr.get_access_token(verifier)
+
 	
 if __name__ == '__main__':
     main()
