8197c6000545d92a667cded03853fe8222f7afe3
Lewis <chlewis@dot.soa.alaska.gov>
2020-09-30 19:12:22 -0800

    Scrapped using flickerapi Implemented authorization using requests_oauthlib instead.

--- change_licensing.py
+++ change_licensing.py
@@ -1,25 +1,57 @@
 
 import config
-import flickrapi
 import webbrowser
+import requests
+from requests_oauthlib import OAuth1
+from oauthlib.common import urldecode
+
 
 def main():
+	flickr = "https://www.flickr.com/services"
+	REQUEST_TOKEN_URL = "https://www.flickr.com/services/oauth/request_token"
+	AUTHORIZE_URL = "https://www.flickr.com/services/oauth/authorize"
+	ACCESS_TOKEN_URL = "https://www.flickr.com/services/oauth/access_token"
+	SET_LICENSE_URL = "https://www.flickr.com/services/rest/?method=flickr.photos.licenses.setLicense"
 	
-	flickr = flickrapi.FlickrAPI(config.API_KEY, config.SECRET)
-	
+		
 	print('Step 1: authenticate')
 	# Get a request token
-	flickr.get_request_token(oauth_callback='oob')
+	oauth = OAuth1(config.API_KEY, config.SECRET)
+	tokencache.SimpleTokenCache()
+	
+	fetch_response = requests.post(REQUEST_TOKEN_URL, data={'oauth_callback':'oob'}, auth=oauth)
+	token = dict(urldecode(fetch_response.text.strip()))
+	
+	print(token)
 
 	# Open a browser at the authentication URL
-	authorize_url = flickr.auth_url(perms='write')
+	authorize_url = "%s?oauth_token=%s&perms=%s" % (AUTHORIZE_URL, token['oauth_token'], 'write')
 	webbrowser.open_new_tab(authorize_url)
 
 	# Get the verifier code from the user
 	verifier = str(input('Verifier code: '))
 
 	# Trade the request token for an access token
-	flickr.get_access_token(verifier)
+	oauth = OAuth1(config.API_KEY,
+                   config.SECRET,
+                   resource_owner_key=token['oauth_token'],
+                   resource_owner_secret=token['oauth_token_secret'],
+                   verifier=verifier)
+				   
+	fetch_response = requests.post(url=ACCESS_TOKEN_URL, auth=oauth)
+	resp = dict(urldecode(fetch_response.text.strip()))
+	print(resp)
+	OWNER_KEY = resp['oauth_token']
+	OWNER_SECRET = resp['oauth_token_secret']
+
+	'''
+	&api_key=8871c6f05ca8b9d979c6fc97191fa999
+	&photo_id=17025391635
+	&license_id=2
+	&format=json
+	&nojsoncallback=1
+	&auth_token=72157716197517768-361e56335c210b2b
+	&api_sig=86424d7dea0e6312417496035687a89d'''
 
 	
 if __name__ == '__main__':
