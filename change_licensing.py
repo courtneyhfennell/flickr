432b7d5f672cc7616543ee1ef7e5fd9e03fc7bad
Lewis <chlewis@dot.soa.alaska.gov>
2020-10-02 07:55:09 -0800

    Adding token caching/retrevial

--- change_licensing.py
+++ change_licensing.py
@@ -4,55 +4,62 @@ import webbrowser
 import requests
 from requests_oauthlib import OAuth1
 from oauthlib.common import urldecode
+import os
+import json
 
+def save_token_to_file(OWNER_KEY, OWNER_SECRET):
+    with open('./config.py', 'a') as f:
+        f.write(f"\nOWNER_KEY = '{OWNER_KEY}'\n")
+        f.write(f"OWNER_SECRET = '{OWNER_SECRET}'\n")
 
-def main():
-	flickr = "https://www.flickr.com/services"
-	REQUEST_TOKEN_URL = "https://www.flickr.com/services/oauth/request_token"
-	AUTHORIZE_URL = "https://www.flickr.com/services/oauth/authorize"
-	ACCESS_TOKEN_URL = "https://www.flickr.com/services/oauth/access_token"
-	SET_LICENSE_URL = "https://www.flickr.com/services/rest/?method=flickr.photos.licenses.setLicense"
-	
-		
-	print('Step 1: authenticate')
-	# Get a request token
-	oauth = OAuth1(config.API_KEY, config.SECRET)
-	tokencache.SimpleTokenCache()
-	
-	fetch_response = requests.post(REQUEST_TOKEN_URL, data={'oauth_callback':'oob'}, auth=oauth)
-	token = dict(urldecode(fetch_response.text.strip()))
-	
-	print(token)
+def get_token():
+    # imaginary API call to get token
+    flickr = "https://www.flickr.com/services"
+    REQUEST_TOKEN_URL = "https://www.flickr.com/services/oauth/request_token"
+    AUTHORIZE_URL = "https://www.flickr.com/services/oauth/authorize"
+    ACCESS_TOKEN_URL = "https://www.flickr.com/services/oauth/access_token"
+    SET_LICENSE_URL = "https://www.flickr.com/services/rest/?method=flickr.photos.licenses.setLicense"
 
-	# Open a browser at the authentication URL
-	authorize_url = "%s?oauth_token=%s&perms=%s" % (AUTHORIZE_URL, token['oauth_token'], 'write')
-	webbrowser.open_new_tab(authorize_url)
+    # Get a request token
+    oauth = OAuth1(config.API_KEY, config.SECRET)
 
-	# Get the verifier code from the user
-	verifier = str(input('Verifier code: '))
+    fetch_response = requests.post(REQUEST_TOKEN_URL, data={'oauth_callback':'oob'}, auth=oauth)
+    token = dict(urldecode(fetch_response.text.strip()))
 
-	# Trade the request token for an access token
-	oauth = OAuth1(config.API_KEY,
-                   config.SECRET,
+    # Open a browser at the authentication URL
+    authorize_url = "%s?oauth_token=%s&perms=%s" % (AUTHORIZE_URL, token['oauth_token'], 'write')
+    webbrowser.open_new_tab(authorize_url)
+
+    # Get the verifier code from the user
+    verifier = str(input('Verifier code: '))
+
+    # Trade the request token for an access token
+    oauth = OAuth1(config.API_KEY,
+                   config.API_SECRET,
                    resource_owner_key=token['oauth_token'],
                    resource_owner_secret=token['oauth_token_secret'],
                    verifier=verifier)
-				   
-	fetch_response = requests.post(url=ACCESS_TOKEN_URL, auth=oauth)
-	resp = dict(urldecode(fetch_response.text.strip()))
-	print(resp)
-	OWNER_KEY = resp['oauth_token']
-	OWNER_SECRET = resp['oauth_token_secret']
 
-	'''
-	&api_key=8871c6f05ca8b9d979c6fc97191fa999
-	&photo_id=17025391635
-	&license_id=2
-	&format=json
-	&nojsoncallback=1
-	&auth_token=72157716197517768-361e56335c210b2b
-	&api_sig=86424d7dea0e6312417496035687a89d'''
+    fetch_response = requests.post(url=ACCESS_TOKEN_URL, auth=oauth)
+    resp = dict(urldecode(fetch_response.text.strip()))
+    
+    save_token_to_file(resp['oauth_token'], resp['oauth_token_secret'])
+    return (resp['oauth_token'], resp['oauth_token_secret'])
 
-	
+
+def main():
+    # Try to get the owner key and secret from the config file, otherwise call the API
+    try:
+        OWNER_KEY = config.OWNER_KEY
+        OWNER_SECRET = config.OWNER_SECRET
+    except AttributeError:
+        OWNER_KEY, OWNER_SECRET = get_token()
+        
+    # queryoauth = OAuth1(config.API_KEY, config.API_SECRET,
+                        # OWNER_KEY, OWNER_SECRET,
+                        # signature_type='query')
+    # r = requests.get(url, auth=queryoauth)
+
+
 if __name__ == '__main__':
     main()
